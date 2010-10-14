a1e529a615b95b5517030b7a3654e2ce5d5cf9a6
Vincent.Z <zhu1230@gmail.com>
2010-10-14 22:07:53 +0800

    add ignores

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "travel"
+set :repository,  "git://github.com/zhu1230/travel.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "doctorthinking.com"                          # Your HTTP server, Apache/etc
+role :app, "doctorthinking.com"                          # This may be the same as your `Web` server
+role :db,  "doctorthinking.com", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+set :deploy_to, "/opt/www/html/travel"
+# set :scm_username,"zhu1230"
+set :user,"root"
+set :use_sudo, false
+set :branch, "master"
+set :deploy_via, :remote_cache
 
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
